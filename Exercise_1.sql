8ca523bb091ac33d9f7465dad6c50143ac902956
Yapnounkie <edmundopierre@yahoo.com>
2022-06-23 21:48:00 -0700

    update Exercice_1

--- Exercise_1.sql
+++ Exercise_1.sql
@@ -1,5 +1,4 @@
 --Instructions
-
 --A :
   --Select the team's long name and API id from the teams_germany table.
   --Filter the query for FC Schalke 04 and FC Bayern Munich using IN, giving you the team_api_IDs needed for the next step.
@@ -10,3 +9,17 @@
 FROM teams_germany
 -- Only include FC Schalke 04 and FC Bayern Munich
 WHERE team_long_name IN ('FC Schalke 04','FC Bayern Munich');
+
+--Instructions
+--B :
+  --Create a CASE statement that identifies whether a match in Germany included FC Bayern Munich, FC Schalke 04, or neither as the home team.
+  --Group the query by the CASE statement alias, home_team.
+
+  SELECT 
+	CASE WHEN hometeam_id = 10189 THEN'FC Schalke 04'
+        WHEN hometeam_id = 9823 THEN 'FC Bayern Munich'
+         ELSE 'Other' END AS home_team,
+	COUNT(id) AS total_matches
+FROM matches_germany
+-- Group by the CASE statement alias
+GROUP BY home_team;
